b8bb7a47209ebf4ee9d80f0f1fb284d2f00526e5
myke amorim <myke.amorim@usp.br>
2023-01-19 01:45:44 -0300

    ORM done

--- src/main/java/com/myke/clients/domain/model/Client.java
+++ src/main/java/com/myke/clients/domain/model/Client.java
@@ -1,14 +1,34 @@
 package com.myke.clients.domain.model;
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Objects;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Entity
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "cname")
     private String name;
     private String email;
-    private String cellNum;
+    private String phone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return getId().equals(client.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
