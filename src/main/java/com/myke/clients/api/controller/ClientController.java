215a16910f908915fb47cc8b90a0c59d2bc592d9
myke amorim <myke.amorim@usp.br>
2023-01-19 01:44:54 -0300

    Changed from EM to repository approach

--- src/main/java/com/myke/clients/api/controller/ClientController.java
+++ src/main/java/com/myke/clients/api/controller/ClientController.java
@@ -1,6 +1,8 @@
 package com.myke.clients.api.controller;
 
 import com.myke.clients.domain.model.Client;
+import com.myke.clients.domain.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+    private ClientRepository clientRepository;
 
     @GetMapping
     public List<Client> list() {
-        Client c1 = new Client(1L, "Myke", "myke.amorim@usp.br", "11959524009");
-        Client c2 = new Client(2L, "Seilo", "exemplasdobblab", "01234567");
-
-        return Arrays.asList(c1, c2);
+        return clientRepository.findAll();
     }
 }
