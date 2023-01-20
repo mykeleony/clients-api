09bfff2c2c6c5c5c9c136a1151c9d4a23b75b83d
myke amorim <myke.amorim@usp.br>
2023-01-20 19:55:30 -0300

    Added get requests on deliveries endpoint

--- src/main/java/com/myke/clients/api/controller/DeliveryController.java
+++ src/main/java/com/myke/clients/api/controller/DeliveryController.java
@@ -2,19 +2,34 @@ package com.myke.clients.api.controller;
 
 import com.myke.clients.domain.model.Delivery;
 import com.myke.clients.domain.service.DeliveryRequestService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/deliveries")
 public class DeliveryController {
+
     private DeliveryRequestService deliveryRequestService;
+
+    @GetMapping
+    public List<Delivery> findAll() {
+        return deliveryRequestService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Delivery> searchId(@PathVariable Long id) {
+        return deliveryRequestService.search(id);
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Delivery request(@RequestBody Delivery delivery) {
+    public Delivery request(@Valid @RequestBody Delivery delivery) {
         return deliveryRequestService.request(delivery);
     }
 
